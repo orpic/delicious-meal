a8cb6fc08bc60d0b4218e5bcc02d939ddab87285
shobhit-u <shobhitumrao5@gmail.com>
2023-01-04 16:02:41 +0530

    assets inititalised in js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className=""></div>;
+  return <div className="">HI</div>;
 }
 
 export default App;
---
+++ src/constants/images.js
@@ -0,0 +1,90 @@
+// App Screenhots
+import appScreen1 from "../assets/app-screen-1.png";
+import appScreen2 from "../assets/app-screen-2.png";
+import appScreen3 from "../assets/app-screen-3.png";
+
+// Customer without name
+import customer1 from "../assets/customer-1.jpg";
+import customer2 from "../assets/customer-2.jpg";
+import customer3 from "../assets/customer-3.jpg";
+import customer4 from "../assets/customer-4.jpg";
+import customer5 from "../assets/customer-5.jpg";
+import customer6 from "../assets/customer-6.jpg";
+
+// Customer with names (for testimonials)
+import customerBen from "../assets/customer-ben.jpg";
+import customerDave from "../assets/customer-dave.jpg";
+import customerHannah from "../assets/customer-hannah.jpg";
+import customerSteve from "../assets/customer-steve.jpg";
+
+// Photos for gallery
+import gallery1 from "../assets/gallery-1.jpg";
+import gallery2 from "../assets/gallery-2.jpg";
+import gallery3 from "../assets/gallery-3.jpg";
+import gallery4 from "../assets/gallery-4.jpg";
+import gallery5 from "../assets/gallery-5.jpg";
+import gallery6 from "../assets/gallery-6.jpg";
+import gallery7 from "../assets/gallery-7.jpg";
+import gallery8 from "../assets/gallery-8.jpg";
+import gallery9 from "../assets/gallery-9.jpg";
+import gallery10 from "../assets/gallery-10.jpg";
+import gallery11 from "../assets/gallery-11.jpg";
+import gallery12 from "../assets/gallery-12.jpg";
+
+// business logos (for featured in section)
+import logoBusinessInsider from "../assets/logo-business-insider.png";
+import logoForbes from "../assets/logo-forbes.png";
+import logoTechCrunch from "../assets/logo-techcrunch.png";
+import logoNewYorkTimes from "../assets/logo-the-new-york-times.png";
+import logoUsaToday from "../assets/logo-usa-today.png";
+
+// misc
+import meal1 from "../assets/meal-1.jpg";
+import meal2 from "../assets/meal-2.jpg";
+import eating from "../assets/misc-eating.jpg";
+import hero from "../assets/misc-hero.png";
+import omniFoodLogo from "../assets/omnifood-logo.png";
+
+// eslint-disable-next-line
+export default {
+  appScreen1,
+  appScreen2,
+  appScreen3,
+
+  customer1,
+  customer2,
+  customer3,
+  customer4,
+  customer5,
+  customer6,
+
+  customerBen,
+  customerDave,
+  customerHannah,
+  customerSteve,
+
+  gallery1,
+  gallery2,
+  gallery3,
+  gallery4,
+  gallery5,
+  gallery6,
+  gallery7,
+  gallery8,
+  gallery9,
+  gallery10,
+  gallery11,
+  gallery12,
+
+  logoBusinessInsider,
+  logoForbes,
+  logoTechCrunch,
+  logoNewYorkTimes,
+  logoUsaToday,
+
+  meal1,
+  meal2,
+  eating,
+  hero,
+  omniFoodLogo,
+};
