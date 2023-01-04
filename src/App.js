47625152419c57c7d42b9c7312b218add5ddfd12
shobhit-u <shobhitumrao5@gmail.com>
2023-01-04 16:35:39 +0530

    header complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import { HeaderSection } from "./components";
 
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <HeaderSection />
+    </div>
+  );
 }
 
 export default App;
--- src/components/HeaderSection/HeaderSection.js
+++ src/components/HeaderSection/HeaderSection.js
@@ -1,8 +1,50 @@
 import React from "react";
 import "./HeaderSection.css";
+import { images } from "../../constants";
 
 const HeaderSection = () => {
-  return <div>HeaderSection</div>;
+  const mainNavList = [
+    {
+      hrefLink: "#how",
+      displayText: "How it works",
+    },
+    {
+      hrefLink: "#meals",
+      displayText: "Meals",
+    },
+    {
+      hrefLink: "#testimonials",
+      displayText: "Testimonials",
+    },
+    {
+      hrefLink: "#pricing",
+      displayText: "Pricing",
+    },
+    {
+      hrefLink: "#cta",
+      displayText: "Try for free",
+    },
+  ];
+
+  return (
+    <header className="header">
+      <a href="#">
+        <img className="logo" alt="Omnifood logo" src={images.omniFoodLogo} />
+      </a>
+
+      <nav className="main-nav">
+        <ul className="main-nav-list">
+          {mainNavList.map((eachItem) => (
+            <li>
+              <a className="main-nav-link" href={eachItem.hrefLink}>
+                {eachItem.displayText}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default HeaderSection;
